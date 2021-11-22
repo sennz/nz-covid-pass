b9e8a03115fd19ed3376accfdc9837b07d3fe905
sennz <sennz@users.noreply.github.com>
2021-11-23 11:18:06 +1300

    Create JWK.php

---
+++ src/Decoder/JWK.php
@@ -0,0 +1,28 @@
+<?php
+namespace SenNZ\NZCovidPass;
+
+use Base64Url\Base64Url;
+use InvalidArgumentException;
+use JsonSerializable;
+
+class JWK implements JsonSerializable
+{
+    /**
+     * @var array
+     */
+    private $values = [];
+
+    /**
+     * Creates a JWK object using the given values.
+     * The member "kty" is mandatory. Other members are NOT checked.
+     */
+    public function __construct(array $values)
+    {
+        if (!isset($values['kty'])) {
+            throw new InvalidArgumentException('The parameter "kty" is mandatory.');
+        }
+        $this->values = $values;
+    }
+
+}
+
