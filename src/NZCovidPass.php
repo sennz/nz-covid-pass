20b51fd381b87aeda28ceb65f9ca68dbf224897f
Sen <sennz@users.noreply.github.com>
2021-11-23 15:44:41 +1300

    Update NZCovidPass.php

--- src/NZCovidPass.php
+++ src/NZCovidPass.php
@@ -5,11 +5,14 @@ namespace SenNZ\NZCovidPass;
 use SenNZ\NZCovidPass\Decoder\NZCovidPassDecoder;
 
 class NZCovidPass {
-   public function __construct($raw_data) {
+   
+   public $data;
+   
+   public function __construct($raw_data,$path="") {
      try {
        $decoder = new NZCovidPassDecoder;
-       $data = $decoder->getNZPassData($raw_data);
-       return $data;
+       $data = $decoder->getNZPassData($raw_data,$path);
+       $this->data = $data;
      } catch (\Exception $e) {
        throw new \InvalidArgumentException('Invalid data');  
      }
